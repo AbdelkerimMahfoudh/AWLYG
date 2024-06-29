b8fef49d7f414a8937d055896ca0b9728c5b52d9
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-06-30 02:20:59 +0500

    finishing the navbar and its style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
+import { useState } from 'react';
 import Awlyg from '../Assets/images/Awlyg.png'
 import { MdMenu } from "react-icons/md";
-const NavBar = () => {
 
+const NavBar = () => {
+    const [active, setActive] = useState('home')
+    const handleChange = (link) => {
+        setActive(link);
+    }
     return(
         <nav className='Navbar'> 
             <ul className='nav-items'>        
@@ -9,11 +14,11 @@ const NavBar = () => {
                     <img src={Awlyg}></img>
                 </li>
                 <li className="headers">
-                    <a href="#">Accueil</a>
-                    <a href="#">Services</a>
-                    <a href="#">Blog</a>
-                    <a href="#">Qui Sommes-nous</a>
-                    <a href="#">Contact</a>
+                    <a href="#home" className={active === 'home' ? 'active' : ''} onClick= { ()=> handleChange('home')}>Accueil</a>
+                    <a href="#services" className={active === 'services' ? 'active' : ''} onClick= { ()=> handleChange('services')}>Services</a>
+                    <a href="#blog" className={active === 'blog' ? 'active' : ''} onClick= { ()=> handleChange('blog')}>Blog</a>
+                    <a href="#about" className={active === 'about' ? 'active' : ''} onClick= { ()=> handleChange('about')}>Qui Sommes-nous</a>
+                    <a href="#contact" className={active === 'contact' ? 'active' : ''} onClick= { ()=> handleChange('contact')}>Contact</a>
                 </li>
                 <li className="toggle-bar">
                     <MdMenu />
