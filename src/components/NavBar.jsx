2be2bd8395914ab7b7e753fdfa808920d0267824
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-06-30 01:19:21 +0500

    a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,25 @@
-
-
+import Awlyg from '../Assets/images/Awlyg.png'
+import { MdMenu } from "react-icons/md";
 const NavBar = () => {
-    
+
     return(
-        <>
-        
-        </>
+        <nav className='Navbar'> 
+            <ul className='nav-items'>        
+                <li className="Logo">
+                    <img src={Awlyg}></img>
+                </li>
+                <li className="headers">
+                    <a href="#">Accueil</a>
+                    <a href="#">Services</a>
+                    <a href="#">Blog</a>
+                    <a href="#">Qui Sommes-nous</a>
+                    <a href="#">Contact</a>
+                </li>
+                <li className="toggle-bar">
+                    <MdMenu />
+                </li>
+            </ul>    
+        </nav>
     )
 }
 
