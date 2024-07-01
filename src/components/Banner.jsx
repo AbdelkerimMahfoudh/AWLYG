5cda025a42dff31c07fa5c9d40965af22a907a88
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-07-02 01:10:40 +0500

    h

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,21 +1,28 @@
-import slider_info from '../Assets/images/slider_info.png';
+import slider_info from '../Assets/images/slider_info.png'
 import { TiMessages } from "react-icons/ti";
-
 const Banner = () => {
+    
     return (
         <div className='banner-container'>
-            <div className="grid-container">
-                <div className="grid-item title">Awlyg Tech - Votre partenaire pour un futur numérique prospère</div>
-                <div className="grid-item image"><img src={slider_info} alt="Slider Info"></img></div>
-                <div className="grid-item news">Actualité et tendance de l'industrie</div>
-                <div className="grid-item icon"><TiMessages /></div>
-                <div className="grid-item contact-text">contact</div>
-                <div className="grid-item contact-email">contact@awlyg.tech</div>
-            </div>
-            <button className='btn-aide'>Besoin d'aide</button>
-            <button className='plus'>En savoir plus</button>
+            <aside>
+                <p>Awlyg Tech - Votre partenaire pour un futur numérique prospère</p>
+                <h1>Actualité et tendance de l'industrie</h1>
+                <div className=''>
+                    
+                    <ul className='banner-contact'>
+                        <li className='msg-icon'><TiMessages/></li>
+                        <li>   
+                            <p>Contact</p>
+                            <a href='#'>contact@awlyg.tech</a>
+                        </li>
+                    </ul>
+                </div>
+                <button className='btn-aide'>Besoin d'aide</button>
+                <button className='plus'>En savoir plus</button>
+            </aside>
+            <img src={slider_info}></img>
         </div>
-    );
+    )
 }
 
-export default Banner;
+export default Banner
