be0f4791a34a65cc476997821e416357e4728fe8
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-07-01 22:13:39 +0500

    d

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Banner from './components/Banner';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer'
 function App() {
   return (
     <>
       <NavBar/>
       <Banner/>
+      <Footer/>
     </>
   );
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,46 +1,21 @@
-import slider_info from '../Assets/images/slider_info.png'
+import slider_info from '../Assets/images/slider_info.png';
 import { TiMessages } from "react-icons/ti";
+
 const Banner = () => {
-    
     return (
         <div className='banner-container'>
-            <table>
-                <thead>
-                    <tr>
-                        <th rowspan="1"><p>Awlyg Tech - Votre partenaire pour un futur numérique prospère</p></th>
-                        <th rowspan="3"><img src={slider_info}></img></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td rowSpan="1"><h1>Actualité et tendance de l'industrie</h1></td>
-                    </tr>
-                    <tr>
-                        <td rowSpan="1"><TiMessages /></td>
-                    </tr>
-                    <tr>
-                        <td rowSpan="1"><p>contact</p></td>
-                        <td rowSpan="1"><h2>contact@awlyg.tech</h2></td>
-                    </tr>
-                </tbody>
-            </table>
-            {/* <
-            <ul>
-                <li></li>
-                <li></li>
-            </ul>
-            <ul>
-                <li>
-                    
-                </li>
-                <li>
-                    
-                </li>
-            </ul>
+            <div className="grid-container">
+                <div className="grid-item title">Awlyg Tech - Votre partenaire pour un futur numérique prospère</div>
+                <div className="grid-item image"><img src={slider_info} alt="Slider Info"></img></div>
+                <div className="grid-item news">Actualité et tendance de l'industrie</div>
+                <div className="grid-item icon"><TiMessages /></div>
+                <div className="grid-item contact-text">contact</div>
+                <div className="grid-item contact-email">contact@awlyg.tech</div>
+            </div>
             <button className='btn-aide'>Besoin d'aide</button>
-            <button className='plus'>En savoir plus</button> */}
+            <button className='plus'>En savoir plus</button>
         </div>
-    )
+    );
 }
 
-export default Banner
+export default Banner;
