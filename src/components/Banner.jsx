bf1307896a62e1bfd48a380401f1b6ee622c35d3
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-07-01 07:43:42 +0500

    adjusting the style

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,22 +4,41 @@ const Banner = () => {
     
     return (
         <div className='banner-container'>
-            <p>Awlyg Tech - Votre partenaire pour un futur numérique prospère</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th rowspan="1"><p>Awlyg Tech - Votre partenaire pour un futur numérique prospère</p></th>
+                        <th rowspan="3"><img src={slider_info}></img></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td rowSpan="1"><h1>Actualité et tendance de l'industrie</h1></td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1"><TiMessages /></td>
+                    </tr>
+                    <tr>
+                        <td rowSpan="1"><p>contact</p></td>
+                        <td rowSpan="1"><h2>contact@awlyg.tech</h2></td>
+                    </tr>
+                </tbody>
+            </table>
+            {/* <
             <ul>
-                <li><h1>Actualité et tendance de l'industrie</h1></li>
-                <li><img src={slider_info}></img></li>
+                <li></li>
+                <li></li>
             </ul>
             <ul>
                 <li>
-                    <TiMessages />
+                    
                 </li>
                 <li>
-                    <p>contact</p>
-                    <h2>contact@awlyg.tech</h2>
+                    
                 </li>
             </ul>
             <button className='btn-aide'>Besoin d'aide</button>
-            <button className='plus'>En savoir plus</button>
+            <button className='plus'>En savoir plus</button> */}
         </div>
     )
 }
