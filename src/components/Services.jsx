3484a41f7890dca0b050d815cc1f2d029d3a43c6
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-07-08 21:17:45 +0500

    adjusting the style of services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,4 +1,8 @@
-import { FiMonitor, FaLongArrowAltRight, CiSettings, LuBrain, LiaMixcloud } from "react-icons/fi";
+import { FiMonitor } from "react-icons/fi";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { CiSettings } from "react-icons/ci";
+import { LuBrain } from "react-icons/lu";
+import { LiaMixcloud } from "react-icons/lia";
 const Services = () => {
     return (
         <div>
@@ -9,28 +13,30 @@ const Services = () => {
                         <li>
                             <h1>01</h1>
                             <p>Creation du solution web et mobiles adaptees</p>
-                            <div className="">
+                            <div className="card-1">
                                 <FiMonitor /> Read more <FaLongArrowAltRight />
                             </div>
                         </li>
                         <li>
-                        <h1>01</h1>
+                        <h1>02</h1>
                             <p>Integration de solutions materielles</p>
-                            <div className="">
+                            <div className="card-2">
                                 <CiSettings /> Read more <FaLongArrowAltRight />
                             </div>
                         </li>
                         <li>
-                        <h1>01</h1>
+                        <h1>03</h1>
                             <p>Solutions d'intelligence artificielle personalisees </p>
-                            <div className="">
-                                <LuBrain /> Read more <FaLongArrowAltRight />
+                            <div className="card-3">
+                                <LuBrain />
+                                Read more 
+                                <button><FaLongArrowAltRight /></button>
                             </div>
                         </li>
                         <li>
-                        <h1>01</h1>
+                        <h1>04</h1>
                             <p>Hebergement et gestion de serveurs cloud</p>
-                            <div className="">
+                            <div className="card-4">
                                 <LiaMixcloud /> Read more <FaLongArrowAltRight />
                             </div>
                         </li>
