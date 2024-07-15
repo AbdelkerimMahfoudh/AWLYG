95ad807eaa39c8d9b916c98372f99eb761c6c343
AbdelkerimMahfoudh <abdelkerim.mahfoudh@gmail.com>
2024-07-15 23:43:51 +0500

    creating the Partners component

---
+++ src/components/Partners.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+const Partners = () => {
+    const [scroll, setScroll] = useState('')
+
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default Partners
